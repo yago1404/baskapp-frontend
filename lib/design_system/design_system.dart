375c64e07b212c0d6ac656d467146f94aae93665
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2025-03-05 10:40:47 -0300

    feat: add exports

--- lib/design_system/design_system.dart
+++ lib/design_system/design_system.dart
@@ -1,8 +1,14 @@
+library;
+
+// CORE
+export 'core/baskapp_sizes.dart';
+
 // ATOMS
 export 'atoms/baskapp_colors.dart';
 export 'atoms/baskapp_text.dart';
 
 // MOLECULES
+export 'molecules/baskapp_input_text.dart';
 
 // ORGANISMS
 
