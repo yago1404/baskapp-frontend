f9cf403d337bcbd81020e45d16bb2d66dc08c43a
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2025-03-05 00:02:06 -0300

    fix: ds position

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:baskapp/core/statics/app_routes.dart';
 import 'package:baskapp/core/statics/app_texts.dart';
-import 'package:baskapp/design_system/baskapp_colors.dart';
+import 'package:baskapp/design_system/atoms/baskapp_colors.dart';
 import 'package:baskapp/modules/auth/auth_module.dart';
 import 'package:flutter/material.dart';
 import 'package:module_navigator/module_navigator.dart';
