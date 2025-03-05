e5381179906dadee80eb2b6fc8a2cdd1ef159876
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2025-03-05 00:05:54 -0300

    feat: exports ds

---
+++ lib/design_system/design_system.dart
@@ -0,0 +1,9 @@
+// ATOMS
+export 'atoms/baskapp_colors.dart';
+export 'atoms/baskapp_text.dart';
+
+// MOLECULES
+
+// ORGANISMS
+
+// TEMPLATES
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:baskapp/core/statics/app_routes.dart';
 import 'package:baskapp/core/statics/app_texts.dart';
-import 'package:baskapp/design_system/atoms/baskapp_colors.dart';
 import 'package:baskapp/modules/auth/auth_module.dart';
 import 'package:flutter/material.dart';
 import 'package:module_navigator/module_navigator.dart';
+
+import 'design_system/design_system.dart';
 
 void main() {
   runApp(const MyApp());
