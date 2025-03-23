c7f34a1fcc519f53e7ee0124468b842229a3e1bd
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2025-03-23 16:02:41 -0300

    feat: change create user page

--- lib/modules/auth/pages/create_user.dart
+++ lib/modules/auth/pages/create_user.dart
@@ -1,3 +1,7 @@
+import 'package:baskapp/core/data/models/enums/position.dart';
+import 'package:baskapp/core/data/models/enums/profile_rule.dart';
+import 'package:baskapp/core/utils/validators_util.dart';
+import 'package:baskapp/design_system/molecules/baskapp_dropdown.dart';
 import 'package:baskapp/modules/auth/view_models/create_profile_view_model.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +17,8 @@ class CreateUserPage extends StatefulWidget {
 }
 
 class _CreateUserPageState extends State<CreateUserPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController =
@@ -20,99 +26,204 @@ class _CreateUserPageState extends State<CreateUserPage> {
 
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _birthdayController = TextEditingController();
-  final TextEditingController _ruleController = TextEditingController();
   final TextEditingController _cellphoneController = TextEditingController();
   final TextEditingController _heightController = TextEditingController();
-  final TextEditingController _positionController = TextEditingController();
+
+  final List<DropdownMenuItem<ProfileRule>> _rules = [
+    DropdownMenuItem<ProfileRule>(
+      value: ProfileRule.coach,
+      child: BaskappText.bodyLarge(ProfileRule.coach.displayName),
+    ),
+    DropdownMenuItem<ProfileRule>(
+      value: ProfileRule.player,
+      child: BaskappText.bodyLarge(ProfileRule.player.displayName),
+    ),
+  ];
+
+  final List<DropdownMenuItem> _positions = [
+    DropdownMenuItem(
+      value: Position.pointGuard,
+      child: BaskappText.bodyLarge(Position.pointGuard.displayName),
+    ),
+    DropdownMenuItem(
+      value: Position.shootingGuard,
+      child: BaskappText.bodyLarge(Position.shootingGuard.displayName),
+    ),
+    DropdownMenuItem(
+      value: Position.smallForward,
+      child: BaskappText.bodyLarge(Position.smallForward.displayName),
+    ),
+    DropdownMenuItem(
+      value: Position.powerForward,
+      child: BaskappText.bodyLarge(Position.powerForward.displayName),
+    ),
+    DropdownMenuItem(
+      value: Position.center,
+      child: BaskappText.bodyLarge(Position.center.displayName),
+    ),
+  ];
+
+  DropdownMenuItem? _selectedRule;
+  DropdownMenuItem? _selectedPosition;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: BaskappColors.primary,
       appBar: AppBar(
         title: BaskappText.titleLarge('Cadastre-se'),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: BaskappSizes.medium,
-            vertical: BaskappSizes.large,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: BaskappSizes.medium,
+          vertical: BaskappSizes.large,
+        ),
+        child: SingleChildScrollView(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              BaskappText.titleLarge('Preencha os dados para criar sua conta'),
-              SizedBox(height: BaskappSizes.large),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: BaskappSizes.medium,
-                children: [
-                  Flexible(
-                    child: Column(
+              Container(
+                decoration: BoxDecoration(
+                  color: BaskappColors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.all(BaskappSizes.medium),
+                width: 500,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BaskappText.titleLarge(
+                      'Preencha os dados para criar sua conta',
+                    ),
+                    SizedBox(height: BaskappSizes.large),
+                    Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: BaskappSizes.small,
                       children: [
-                        BaskappText.titleMedium('Dados de login'),
-                        BaskappInputText(
-                          controller: _emailController,
-                          label: 'Email',
-                        ),
-                        BaskappInputText(
-                          controller: _passwordController,
-                          label: 'Senha',
-                        ),
-                        BaskappInputText(
-                          controller: _confirmPasswordController,
-                          label: 'Confirme sua senha',
+                        Flexible(
+                          child: Form(
+                            key: _formKey,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              spacing: BaskappSizes.small,
+                              children: [
+                                BaskappText.titleMedium('Dados de login'),
+                                BaskappInputText(
+                                  controller: _emailController,
+                                  validator: ValidatorsUtil.validateEmail,
+                                  label: 'Email',
+                                ),
+                                BaskappInputText(
+                                  controller: _passwordController,
+                                  validator: ValidatorsUtil.validatePassword,
+                                  label: 'Senha',
+                                ),
+                                BaskappInputText(
+                                  controller: _confirmPasswordController,
+                                  validator: _validateConfirmationPassword,
+                                  label: 'Confirme sua senha',
+                                ),
+                                SizedBox(height: BaskappSizes.common),
+                                BaskappText.titleMedium('Dados de perfil'),
+                                BaskappInputText(
+                                  controller: _nameController,
+                                  validator: ValidatorsUtil.validateFullName,
+                                  label: 'Nome completo',
+                                ),
+                                Row(
+                                  spacing: BaskappSizes.small,
+                                  children: [
+                                    Flexible(
+                                      child: BaskappInputText(
+                                        controller: _birthdayController,
+                                        label: 'Data de nascimento',
+                                        hintText: 'DD/MM/AAAA',
+                                      ),
+                                    ),
+                                    Flexible(
+                                      child: BaskappDropdown(
+                                        items: _rules,
+                                        hintText:
+                                            _selectedRule?.value.displayName ??
+                                            'Tipo de perfil',
+                                        onChange: _changeRule,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                if (_selectedRule?.value ==
+                                    ProfileRule.player) ...[
+                                  Row(
+                                    spacing: BaskappSizes.small,
+                                    children: [
+                                      Flexible(
+                                        flex: 1,
+                                        child: BaskappInputText(
+                                          controller: _heightController,
+                                          hintText: 'Ex.: 1,85',
+                                          label: 'Altura',
+                                        ),
+                                      ),
+                                      Flexible(
+                                        flex: 3,
+                                        child: BaskappInputText(
+                                          controller: _cellphoneController,
+                                          label: 'Telefone para contato',
+                                          hintText: '(11) 9 9999-9999',
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  BaskappDropdown(
+                                    items: _positions,
+                                    hintText:
+                                        _selectedPosition?.value.displayName ??
+                                        'Posição em quadra',
+                                    onChange: _changePosition,
+                                  ),
+                                ],
+                                SizedBox(height: BaskappSizes.medium),
+                                BaskappButton(
+                                  'Cadastrar',
+                                  onPressed: _onPressedRegister,
+                                  expanded: true,
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                  Flexible(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: BaskappSizes.small,
-                      children: [
-                        BaskappText.titleMedium('Dados de perfil'),
-                        BaskappInputText(
-                          controller: _nameController,
-                          label: 'Nome completo',
-                        ),
-                        BaskappInputText(
-                          controller: _birthdayController,
-                          label: 'Data de nascimento',
-                          hintText: 'DD/MM/AAAA',
-                        ),
-                        BaskappInputText(
-                          controller: _ruleController,
-                          label: 'Competencia',
-                          hintText: 'TECNICO ou JOGADOR',
-                        ),
-                        BaskappInputText(
-                          controller: _heightController,
-                          label: 'Altura',
-                        ),
-                        BaskappInputText(
-                          controller: _positionController,
-                          label: 'Posicao em quadra',
-                        ),
-                        BaskappInputText(
-                          controller: _cellphoneController,
-                          label: 'Telefone para contato',
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-              SizedBox(height: BaskappSizes.extraLarge),
-              BaskappButton('Cadastrar', onPressed: () => {}, expanded: true),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void _changeRule(value) => setState(() {
+    _selectedRule = _rules.firstWhere((e) => e.value == value);
+  });
+
+  void _changePosition(value) => setState(() {
+    _selectedPosition = _positions.firstWhere((e) => e.value == value);
+  });
+
+  void _onPressedRegister() {
+    if (_formKey.currentState!.validate()) {
+
+    }
+  }
+
+  String? _validateConfirmationPassword(String? value) {
+    if (value != _passwordController.text) {
+      return 'As senhas devem ser iguais';
+    }
+
+    return null;
   }
 }
